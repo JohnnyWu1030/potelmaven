e091fa00d558885db3e1e02d900ca8e38af08da8
JohnnyWu <orion_wu@yahoo.com.tw>
2024-12-25 13:53:21 +0800

    First test

---
+++ src/main/java/test/controller/TestController.java
@@ -0,0 +1,20 @@
+package test.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestController extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html;charset=UTF-8");
+		resp.getWriter().write("趕快下課");
+	}
+}
